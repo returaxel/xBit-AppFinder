e126d4880c7768af7651df4f79c38fd74ce24c08
returaxel <48722741+returaxel@users.noreply.github.com>
2022-10-26 12:35:14 +0200

    Rewritten as xBit-AppFinder

--- MSI-AppAnnihilator.ps1
+++ MSI-AppAnnihilator.ps1
@@ -1,78 +1,112 @@
-﻿# Detect BigFix Client (1/2)
+﻿# Find 64-bit application guid by DisplayName
+#
+#
 
 param(
-    [Parameter()][string]$Application,
-    [Parameter()][string]$Arguments = "/qn",
-    [Parameter()][switch]$Uninstall
+    [Parameter(Mandatory=$true)][string]$Application,
+    [Parameter()][switch]$Uninstall,
+    [Parameter()][double]$TargetVer
 )
 
-# Logging
-$ExportPath = edit me
-$NetworkAccess = Test-Path $ExportPath 
-
-$HostInfo = [PSCustomObject]@{
-    Date = '{0}{1}{2}' -f [datetime]::now.ToShortDateString(), ' ', [datetime]::now.ToShortTimeString()
-    Hostname = $env:COMPUTERNAME
-    DisplayName = $null
-    UninstallString = $null
-    ExitCode = $null
+# Stop if $Application is empty
+if ([string]::IsNullOrEmpty($Application)) {
+    return Write-Host "No application selected"
 }
 
-# Find 64-bit application guid by DisplayName
-function Get-ApplicationGuid {
+# --- Function: REGISTRY SCANNER ---
+function Get-RegMatches {
     param (
-        [Parameter()][string]$TargetApp
+        [Parameter()][string]$PathHKLM,
+        [Parameter()][array]$KeysHKLM,
+        [Parameter()][array]$StrMatch
     )
 
-    if ([string]::IsNullOrEmpty($TargetApp)) {
-        return Write-Host "No application selected"
-    }
+    [PSCustomObject]$AppMatches = foreach ($key in $KeysHKLM) {
 
-    # Registry
+        try {
+            $SubKeys = Get-ItemProperty -Verbose -Path $PathHKLM$key
+        } catch {}
+
+        $reg = switch -regex ($PathHKLM) {
+            'WOW6432Node'   {64}
+            Default         {32}
+        }
+        # Match selected application to DisplayName
+        $Regex = [regex]::Match($SubKeys.DisplayName,$($Application))
+        if (($Regex.Success -eq $true) -and (![string]::IsNullOrEmpty($SubKeys.DisplayName))) {
+
+
+            [PSCustomObject]@{
+                RegPath = "$($reg)bit"
+                RegexMatch = $Regex.Value
+                DisplayName = $SubKeys.DisplayName
+                DisplayVersion = $SubKeys.DisplayVersion
+                VerMajor = $SubKeys.VersionMajor
+                VerMinor = $SubKeys.VersionMinor
+                UninstallString = [regex]::Match($SubKeys.UninstallString,'[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?').Value
+                ExitCode = $null
+            }
+        }
+    }
+    return $AppMatches
+}
+
+# --- Function: UNINSTALL ---
+function Remove-ThisApplication {
+    param (
+        [Parameter()][pscustomobject]$TargetApp,
+        [Parameter()][double]$RemVer
+    )
+
+    if (![string]::IsNullOrEmpty($TargetApp.UninstallString)) {
+        [double]$CurrentVer = '{0}{1}{2}' -f $TargetApp.VerMajor,'.', $TargetApp.Verminor
+        Write-Host "[Version]`nMajor: $($TargetApp.VerMajor)`nMinor: $($TargetApp.VerMinor)`ndouble: $($CurrentVer)" -ForegroundColor Cyan # Debug
+        Write-Host "TargetVer: $RemVer `nIsLower: $($RemVer -le $CurrentVer)" -ForegroundColor Magenta # Debug
+
+        if ($RemVer -le $CurrentVer ) {
+            Write-Host "Uninstall: true" -ForegroundColor Green # Debug
+            # Uninstall
+            try {
+                $ExitCode = (Start-Process msiexec.exe -ArgumentList "/X $($TargetApp.UninstallString) /qn" -Wait -Verb RunAs -Passthru).ExitCode
+                return $ExitCode
+            }
+            catch {
+                return "Interrupted"
+            }
+        }
+    }
+}
+
+function Find-ThisApplication {
+    param (
+        [Parameter()][string]$Application
+    )
+
+    # Path & Keys
+    $HKLM32Uninst = "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\"
     $HKLM64Uninst = "hklm:\\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\"
+    $KeysHKLM32 = (Get-Item $HKLM32Uninst).GetSubKeynames()
     $KeysHKLM64 = (Get-Item $HKLM64Uninst).GetSubKeynames()
 
-    # Iterate all installed 64-bit applications
-    foreach ($key in $KeysHKLM64) {
-
-        try {
-             $SubKeys = Get-ItemProperty -Verbose -Path $HKLM64Uninst$key 
-        } 
-        catch {}
-    
-        if ($SubKeys.DisplayName -match "$($TargetApp)") {
-        # Write information to HostInfo
-            $HostInfo.DisplayName = $SubKeys.DisplayName
-            $HostInfo.UninstallString = [regex]::Match($SubKeys.UninstallString,'[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?').Value
-            return
-        }
+    $FoundApps = [PSCustomObject]@{
+        32 = Get-RegMatches -PathHKLM $HKLM32Uninst -KeysHKLM $KeysHKLM32 -StrMatch $Application
+        64 = Get-RegMatches -PathHKLM $HKLM64Uninst -KeysHKLM $KeysHKLM64 -StrMatch $Application
     }
+    return $FoundApps
 }
 
-# Uninstall application 
-function Uninstall-ThisApplication {
-    param (
-        [Parameter()][string]$TargetGuid,
-        [parameter()][string]$Arguments
-    )
+# --- Script ---
+$InfoArray = Find-ThisApplication $Application
 
-    if (![string]::IsNullOrEmpty($TargetGuid)) {
-        try {
-            $ExitCode = (Start-Process msiexec.exe -ArgumentList "/X$($TargetGuid) $($Arguments)" -Wait -Verb RunAs -Passthru).ExitCode
-        }
-        catch {
-            return "Uninstall cancelled."
-        }
-        return $ExitCode
-    }
-}
-
-Get-ApplicationGuid $Application
-
+# Uninstall:$true
 if ($Uninstall) {
-    $HostInfo.ExitCode = Uninstall-ThisApplication $HostInfo.UninstallString $Arguments
+    # Iterate 32 & 64 
+    foreach ($arch in $InfoArray.psobject.Properties.name) {
+        # and then each found app
+        foreach ($App in $InfoArray.$arch) {
+            $App.ExitCode = Remove-ThisApplication $App $TargetVer
+        }
+    }
 }
 
-# Write info to console
-$HostInfo | Format-Table
-$HostInfo | Export-Csv "$($ExportPath)\$($env:COMPUTERNAME).csv" -NoTypeInformation -Delimiter "," -Append
+$InfoArray #| Format-Table -AutoSize
