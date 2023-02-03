4349abba9c067bc8bd458331b38e520cf28d5955
returaxel <48722741+returaxel@users.noreply.github.com>
2023-02-03 17:30:07 +0100

    Update xBit-AppFinder-Uninstall.ps1

--- xBit-AppFinder-Uninstall.ps1
+++ xBit-AppFinder-Uninstall.ps1
@@ -3,8 +3,10 @@
     - Find installed application GUID
 .PARAMETER Application
     - Search registry for this application
+.PARAMETER RegexWhiteList
+    - Skip uninstall if matches displayname
 .PARAMETER Uninstall test
-    - Attempt to uninstall *every* application found
+    - Switch for uninstalling found applicaitons
 .EXAMPLE
     - Lookup installed applications
     - .\xBit-AppFinder-Uninstall.ps1 -Application <appname>
@@ -14,16 +16,53 @@
 
 param(
     [Parameter(Mandatory=$true)][string]$Application,
+    [Parameter()][string]$RegexWhitelist = '^Touchpad|Peripheral',
     [Parameter()][switch]$Uninstall
 )
 
-function xBit-AppUninstall {
+function xBit-TerminateProc {
+    param (
+        [Parameter(Mandatory=$true)][pscustomobject]$ProcessName
+    )
+    # Terminate $Application process, enter manually if needed
+    while ((Get-Process $Application -ErrorAction SilentlyContinue).processName -contains $Application) {
+        try {
+            taskkill /IM "$($Application).exe" /T /F
+            Start-Sleep -Milliseconds 500
+        } catch {
+            Write-Host "Could not kill process for $Application" -ForegroundColor Yellow
+        }
+    }
+}
+
+function xBit-Uninstall {
     param (
         [Parameter()][pscustomobject]$InputObject
     )
 
     foreach ($app in $InputObject) {
-        $app.ExitCode = (Start-Process msiexec.exe -ArgumentList "/X $([regex]::Match($app.UninstallString,'[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?').Value) /qn" -Wait -Verb RunAs -Passthru).ExitCode
+        if ($app.DisplayName -notmatch $RegexWhitelist) {
+
+        # Run if UninstallString is not empty
+            if (![string]::IsNullOrEmpty($app.UninstallString)) {
+
+                try {
+                    $app.ExitCode = (Start-Process msiexec.exe -ArgumentList "/x $([regex]::Match($app.UninstallString,'[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?').Value) /qn" -Wait -Verb RunAs -Passthru).ExitCode
+                }
+                catch {
+                    $app.ExitCode = $PSItem.Exception.Message
+                } 
+                finally {
+                    $Error.Clear()
+                }
+
+            } else {
+                $app.ExitCode = 'No UninstallString'
+            }
+        } else {
+            $app.ExitCode=  "Skip, match whitelist."
+        }
+
         return $InputObject
     }
 }
@@ -38,19 +77,11 @@ $xBitApps = $HKLMUninst | Get-ItemProperty | Where-Object {$_.DisplayName -match
 if ($Uninstall) {
     Write-Host "`n[ Uninstall ]" -ForegroundColor Green
 
-    <# Terminate $Application process if needed, add proc name manually if needed
-    while ((Get-Process $Application -ErrorAction SilentlyContinue).processName -contains $Application) {
-        try {
-            taskkill /IM "$($Application).exe" /T /F
-            Start-Sleep -Milliseconds 500
-        } catch {
-            Write-Host "Could not kill process for $Application" -ForegroundColor Yellow
-            exit 1
-        }
-    }#>
+    # Comment out if process don't need to be terminated
+    xBit-TerminateProc -ProcessName $Application
 
     if ($xBitApps.Count -ne 0) {
-        $xBitApps = xBit-AppUninstall $xBitApps
+        $xBitApps = xBit-Uninstall $xBitApps
     }
 }
 
